8ff651e69c7555127ea96e86c884007a4758c25e
engin0223 <engineda2014@hotmail.com>
2025-12-16 18:27:40 +0300

    Add a check if the manager is already open. If it is, switch to it; otherwise, open it.

--- extension_tabmanager/background.js
+++ extension_tabmanager/background.js
@@ -5,7 +5,18 @@
  * Opens the extension's UI page (ui.html) in a new tab when the user clicks
  * the extension icon in the browser toolbar.
  */
-chrome.action.onClicked.addListener(() => {
+chrome.action.onClicked.addListener(async () => {
   const packagedUrl = chrome.runtime.getURL('ui.html');
-  chrome.tabs.create({ url: packagedUrl });
+  // Query to see if the tab is already open
+  const tabs = await chrome.tabs.query({ url: packagedUrl });
+
+  if (tabs.length > 0) {
+    // If found, update the window and tab to focus it
+    const tab = tabs[0];
+    await chrome.windows.update(tab.windowId, { focused: true });
+    await chrome.tabs.update(tab.id, { active: true });
+  } else {
+    // Otherwise create it
+    chrome.tabs.create({ url: packagedUrl });
+  }
 });
